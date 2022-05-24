5ad1ed16fdeada0fc340888e873df4f63638b00e
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-05-24 13:48:05 +0100

    move ueventd to TARGET_VENDOR_OUT/etc

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -30,7 +30,8 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.samsungexynos7420.rc
+LOCAL_MODULE_STEM := ueventd.rc
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR)/etc
 include $(BUILD_PREBUILT)
